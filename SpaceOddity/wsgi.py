17ffa1495a1220e4387bd3a562118917a431639b
kingagulewska <kinga.gulewska@gmail.com>
2017-11-21 19:50:24 +0100

    wsgi file for python anywhere

--- SpaceOddity/wsgi.py
+++ SpaceOddity/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
 import os
-from dj_static import Cling
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/kingag/SpaceOddity-KG/'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'SpaceOddity.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SpaceOddity.settings")
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+###### or, for older Django <=1.4
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
